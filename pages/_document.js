79ec6bdc3e912f0280e2328ca5df03eb1c796f81
thaohoang255 <71319905+thaohoang255@users.noreply.github.com>
2026-04-18 23:41:34 +0700

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="vi">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="HSK3" />
+        <meta name="theme-color" content="#ef4444" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
